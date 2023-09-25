e9ea7a593e91c88ef0aa3c257f379c86c037b3d6
MortenNie <cph-mn705@cphbusiness.dk>
2023-09-25 12:39:11 +0200

    KAGE

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,7 +1,43 @@
 package dat;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "https://vejr.tv2.dk/vejr/koebenhavn-2618425";
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            /* for (Element row : document.select("table.tc_datatable_main td")) {
+
+                 String weather = row.select("tc_weather_forecast_list_temperature").text();
+                 System.out.println(weather);
+             } */
+
+            var table = document.select("table").get(0);
+            var rows = table.select("tr");
+
+            for (Element s: rows) {
+                var tempWind =  s.select("td").text();
+                var time = s.select ("th").text();
+                var precipitation = s.select("td.tc_weather_forecast_list_precipitation").text();
+                if (precipitation.equals("")) {
+                    precipitation = "0";
+
+                }
+                double precipitationFinal = Double.parseDouble(precipitation);
+
+                System.out.println(time + " - " + tempWind + " - " + precipitationFinal + "mm");
+            }
+
+
+
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+
+        }
     }
 }
---
+++ src/main/java/dat/WeatherDataDTO.java
@@ -0,0 +1,19 @@
+package dat;
+
+import java.time.LocalDate;
+
+public class WeatherDataDTO {
+    private String time;
+
+    private LocalDate date;
+
+    private double temperatur;
+
+    private String wind;
+
+    private double precipitation;
+
+
+
+
+}
